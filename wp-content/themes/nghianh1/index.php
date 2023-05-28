870083eb177de5d9b6a7d53b5504c28f3747ba2c
Nghia Nguyen <nghianguyen@MacBook-Pro.local>
2023-05-28 15:12:57 +0700

    theme - global style - processing

--- wp-content/themes/nghianh1/index.php
+++ wp-content/themes/nghianh1/index.php
@@ -1,13 +1,13 @@
 <!doctype html>
 <html <?php language_attributes(); ?> >
 <head>
-    <meta charset="<?php echo bloginfo('charset') ?>" >
-    <?php echo wp_head(); ?> <!--allow wp add header-->
+    <meta charset="<?php bloginfo('charset') ?>" >
+    <?php wp_head(); ?> <!--allow wp add header-->
 </head>
-<body <?php echo body_class('sample'); ?> >
-    <?php echo wp_body_open(); ?> <!--allow add wp hook-->
+<body <?php body_class('sample-acb'); ?> >
+    <?php wp_body_open(); ?> <!--allow add wp hook-->
     <p>Hello Nghia!</p>
 
-    <?php echo wp_footer(); ?> <!--allow wp add footer-->
+    <?php wp_footer(); ?> <!--allow wp add footer-->
 </body>
 </html>
